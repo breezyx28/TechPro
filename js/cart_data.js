d86c1c7b51927bdbfe1a86154b7954e65d431419
Mohamed Ahamed <mohamedx.28@gmail.com>
2020-06-15 17:23:46 +0100

    update category and cart page

--- js/cart_data.js
+++ js/cart_data.js
@@ -13,38 +13,95 @@ $(document).ready(function () {
   $.getJSON('../json/category.json', function (data) {
     $.each(data, function (index, value) {
       items.push(data);
-      clo.push(value.clothes);
-      elec = value.Electronics;
-      furn = value.Furniture;
-      food = value.food;
+      // clo.push(value.clothes);
+      // elec = value.Electronics;
+      // furn = value.Furniture;
+      // food = value.food;
     });
   });
 
-  function contents() {
+  function contents(cat) {
     setTimeout(function () {
       $.each(items, function (index, value) {
-        console.log(value.category.clothes[code]);
-        $('.cart_main_desc_title').html(value.category.clothes[code].name);
-        let price = $('.cart_main_desc_price').attr(
-          'value',
-          value.category.clothes[code].price,
-        );
-        $('.cart_main_desc_price').html(
-          '$' + value.category.clothes[code].price,
-        );
-        $('.cart_mian_img_holder > img').attr(
-          'src',
-          value.category.clothes[code].image_url,
-        );
+        if (cat == 'clothes') {
+          console.log(value.category.clothes[code]);
+          $('.cart_main_desc_title').html(value.category.clothes[code].name);
+          let price = $('.cart_main_desc_price').attr(
+            'value',
+            value.category.clothes[code].price,
+          );
+          $('.cart_main_desc_price').html(
+            '$' + value.category.clothes[code].price,
+          );
+          $('.cart_mian_img_holder > img').attr(
+            'src',
+            value.category.clothes[code].image_url,
+          );
+        }
+        if (cat == 'furniture') {
+          console.log(value.category.Furniture[code]);
+          $('.cart_main_desc_title').html(value.category.Furniture[code].name);
+          let price = $('.cart_main_desc_price').attr(
+            'value',
+            value.category.Furniture[code].price,
+          );
+          $('.cart_main_desc_price').html(
+            '$' + value.category.Furniture[code].price,
+          );
+          $('.cart_mian_img_holder > img').attr(
+            'src',
+            value.category.Furniture[code].image_url,
+          );
+        }
+        if (cat == 'Electronics') {
+          console.log(value.category.Electronics[code]);
+          $('.cart_main_desc_title').html(
+            value.category.Electronics[code].name,
+          );
+          let price = $('.cart_main_desc_price').attr(
+            'value',
+            value.category.Electronics[code].price,
+          );
+          $('.cart_main_desc_price').html(
+            '$' + value.category.Electronics[code].price,
+          );
+          $('.cart_mian_img_holder > img').attr(
+            'src',
+            value.category.Electronics[code].image_url,
+          );
+        }
+        if (cat == 'food') {
+          console.log(value.category.food[code]);
+          $('.cart_main_desc_title').html(value.category.food[code].name);
+          let price = $('.cart_main_desc_price').attr(
+            'value',
+            value.category.food[code].price,
+          );
+          $('.cart_main_desc_price').html(
+            '$' + value.category.food[code].price,
+          );
+          $('.cart_mian_img_holder > img').attr(
+            'src',
+            value.category.food[code].image_url,
+          );
+        }
       });
     }, 1000);
   }
 
   switch (category) {
     case 'clothes':
-      contents();
+      contents('clothes');
       break;
-
+    case 'furniture':
+      contents('furniture');
+      break;
+    case 'food':
+      contents('food');
+      break;
+    case 'Electronics':
+      contents('Electronics');
+      break;
     default:
       break;
   }
